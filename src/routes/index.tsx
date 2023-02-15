4e72f82ae062cf1b23078f3a6a1be6bc219fa070
Luke-lwz <lukaslunkwitz@gmail.com>
2023-02-15 11:03:36 +0100

    lol

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { component$ } from '@builder.io/qwik';
-import { DocumentHead, Link } from '@builder.io/qwik-city';
+import { DocumentHead } from '@builder.io/qwik-city';
 
 import LinkTree from './LinkTree';
 
